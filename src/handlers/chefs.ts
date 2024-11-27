24d1fff3acc1feff0d0a7ba7f38bc38073428c4b
Hagai Hen <hagaih@moveo.co.il>
2024-11-27 15:41:21 +0200

    remove try and catch from handlers

--- src/handlers/chefs.ts
+++ src/handlers/chefs.ts
@@ -1,104 +1,67 @@
 import Chef from "../models/Chef";
-import {
-  checkCreateChef,
-  checkUpdateChef,
-} from "../services/chefs";
+import { checkCreateChef, checkUpdateChef } from "../services/chefs";
 import { ChefInterface } from "../interfaces";
 
 export const getAllChefsHandler = async () => {
-  try {
-    const chefs = await Chef.find({});
-    if (!chefs) {
-      throw new Error("Chefs are empty");
-    }
-    return chefs;
-  } catch (error) {
-    console.log(
-      "error in getAllChefsHandler: ",
-      (error as Error).message
-    );
-    throw error;
+  const chefs = await Chef.find({});
+  if (!chefs) {
+    throw new Error("Chefs are empty");
   }
+  return chefs;
 };
 
 export const getChefHandler = async (id: string) => {
-  try {
-    const chef = await Chef.findById(id);
-    if (!chef) {
-      throw new Error("Chef not exists");
-    }
-    return chef;
-  } catch (error) {
-    console.log("error in getChefHandler: ", (error as Error).message);
-    throw error;
+  const chef = await Chef.findById(id);
+  if (!chef) {
+    throw new Error("Chef not exists");
   }
+  return chef;
 };
 
 export const updateChefHandler = async (
   id: string,
   updateData: ChefInterface
 ) => {
-  try {
-    const chef = await Chef.findById(id);
-    if (!chef) {
-      throw new Error("Chef not exists");
-    }
-    
-    checkUpdateChef(updateData);
-
-    const updatedChef = await Chef.findByIdAndUpdate(
-      id,
-      updateData,
-      {
-        new: true,
-      }
-    );
-    return updatedChef;
-  } catch (error) {
-    console.log("error in updateChefHandler: ", (error as Error).message);
-    throw error;
+  const chef = await Chef.findById(id);
+  if (!chef) {
+    throw new Error("Chef not exists");
   }
+
+  checkUpdateChef(updateData);
+
+  const updatedChef = await Chef.findByIdAndUpdate(id, updateData, {
+    new: true,
+  });
+  return updatedChef;
 };
 
 export const deleteChefHandler = async (id: string) => {
-  try {
-    const chef = await Chef.findByIdAndDelete(id);
-    if (!chef) {
-      throw new Error("Chef not exists");
-    }
-    return chef;
-  } catch (error) {
-    console.log("error in deleteChefHandler: ", (error as Error).message);
-    throw error;
+  const chef = await Chef.findByIdAndDelete(id);
+  if (!chef) {
+    throw new Error("Chef not exists");
   }
+  return chef;
 };
 
-export const createChefHandler = async (
-  chef: ChefInterface
-) => {
-  try {
-    if (!chef) {
-      throw new Error("Chef not valid");
-    }
-
-    checkCreateChef(chef);
-
-    const newChef = new Chef({
-      name: chef.name,
-      img: chef.img,
-      description: chef.description,
-      restaurants: chef.restaurants,
-  })
-
-    if (!newChef) {
-      throw new Error("can't create new chef");
-    }
-
-    await newChef.save();
-
-    return newChef;
-  } catch (error) {
-    console.log("error in createChefHandler: ", (error as Error).message);
-    throw error;
+export const createChefHandler = async (chef: ChefInterface) => {
+  if (!chef) {
+    throw new Error("Chef not valid");
   }
+
+  checkCreateChef(chef);
+
+  const newChef = new Chef({
+    name: chef.name,
+    img: chef.img,
+    description: chef.description,
+    restaurants: chef.restaurants,
+  });
+
+  if (!newChef) {
+    throw new Error("can't create new chef");
+  }
+
+  await newChef.save();
+
+  return newChef;
 };
--- src/handlers/restaurants.ts
+++ src/handlers/restaurants.ts
@@ -6,99 +6,67 @@ import {
 import { RestaurantInterface } from "../interfaces";
 
 export const getAllRestaurantsHandler = async () => {
-  try {
-    const restaurants = await Restaurant.find({});
-    if (!restaurants) {
-      throw new Error("Restaurants are empty");
-    }
-    return restaurants;
-  } catch (error) {
-    console.log(
-      "error in getAllRestaurantsHandler: ",
-      (error as Error).message
-    );
-    throw error;
+  const restaurants = await Restaurant.find({});
+  if (!restaurants) {
+    throw new Error("Restaurants are empty");
   }
+  return restaurants;
 };
 
 export const getRestaurantHandler = async (id: string) => {
-  try {
-    const restaurant = await Restaurant.findById(id);
-    if (!restaurant) {
-      throw new Error("Restaurant not exists");
-    }
-    return restaurant;
-  } catch (error) {
-    console.log("error in getRestaurantHandler: ", (error as Error).message);
-    throw error;
+  const restaurant = await Restaurant.findById(id);
+  if (!restaurant) {
+    throw new Error("Restaurant not exists");
   }
+  return restaurant;
 };
 
 export const updateRestaurantHandler = async (
   id: string,
   updateData: RestaurantInterface
 ) => {
-  try {
-    const restaurant = await Restaurant.findById(id);
-    if (!restaurant) {
-      throw new Error("Restaurant not exists");
-    }
-    
-    checkUpdateRestaurant(updateData);
-
-    const updatedRestaurant = await Restaurant.findByIdAndUpdate(
-      id,
-      updateData,
-      {
-        new: true,
-      }
-    );
-    return updatedRestaurant;
-  } catch (error) {
-    console.log("error in updateRestaurantHandler: ", (error as Error).message);
-    throw error;
+  const restaurant = await Restaurant.findById(id);
+  if (!restaurant) {
+    throw new Error("Restaurant not exists");
   }
+
+  checkUpdateRestaurant(updateData);
+
+  const updatedRestaurant = await Restaurant.findByIdAndUpdate(id, updateData, {
+    new: true,
+  });
+  return updatedRestaurant;
 };
 
 export const deleteRestaurantHandler = async (id: string) => {
-  try {
-    const restaurant = await Restaurant.findByIdAndDelete(id);
-    if (!restaurant) {
-      throw new Error("Restaurant not exists");
-    }
-    return restaurant;
-  } catch (error) {
-    console.log("error in deleteRestaurantHandler: ", (error as Error).message);
-    throw error;
+  const restaurant = await Restaurant.findByIdAndDelete(id);
+  if (!restaurant) {
+    throw new Error("Restaurant not exists");
   }
+  return restaurant;
 };
 
 export const createRestaurantHandler = async (
   restaurant: RestaurantInterface
 ) => {
-  try {
-    if (!restaurant) {
-      throw new Error("Restaurant not valid");
-    }
-
-    checkCreateRestaurant(restaurant);
-
-    const newRestaurant = new Restaurant({
-      name: restaurant.name,
-      img: restaurant.img,
-      chef: restaurant.chef,
-      dishes: restaurant.dishes,
-    });
-
-    if (!newRestaurant) {
-      throw new Error("can't create new restaurant");
-    }
-
-    await newRestaurant.save();
-
-    return newRestaurant;
-  } catch (error) {
-    console.log("error in createRestaurantHandler: ", (error as Error).message);
-    throw error;
+  if (!restaurant) {
+    throw new Error("Restaurant not valid");
   }
+
+  checkCreateRestaurant(restaurant);
+
+  const newRestaurant = new Restaurant({
+    name: restaurant.name,
+    img: restaurant.img,
+    chef: restaurant.chef,
+    dishes: restaurant.dishes,
+  });
+
+  if (!newRestaurant) {
+    throw new Error("can't create new restaurant");
+  }
+
+  await newRestaurant.save();
+
+  return newRestaurant;
 };
